d4df6680a114fc532bef8ab17f0129cca04d535d
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-14 23:50:24 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 
 st.title('Welcome to the Machine learning Model of Prosper Loan Data')
+st.subheader('Developed by Aashutosh Sinha')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
