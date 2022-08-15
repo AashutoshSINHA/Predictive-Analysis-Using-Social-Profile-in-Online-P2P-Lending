92254f8737498850e1ee3cd9d8e6c5c1a01b18b0
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-15 23:29:02 +0545

    Update streamlit_try.py

--- streamlit_try.py
+++ streamlit_try.py
@@ -1,3 +1,23 @@
 import streamlit as st
 with st.sidebar:
     add_radio = st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as a PIL Image:
+    img = Image.open(img_file_buffer)
+
+    # To convert PIL Image to numpy array:
+    img_array = np.array(img)
+
+    # Check the type of img_array:
+    # Should output: <class 'numpy.ndarray'>
+    st.write(type(img_array))
+
+    # Check the shape of img_array:
+    # Should output shape: (height, width, channels)
+    st.write(img_array.shape)
