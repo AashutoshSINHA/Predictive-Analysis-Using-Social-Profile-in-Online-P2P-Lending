3bd2490cf3d5c97c00dfb065541d0b6120884fb7
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-15 15:59:03 +0545

    Updated streamlit.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,28 @@ import pandas as pd
 from pandas import DataFrame
 st.title('Welcome to the Machine learning Model of Prosper Loan Data')
 st.subheader('Developed by Aashutosh Sinha')
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.getvalue()
+     st.write(bytes_data)
 
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
+      uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
 
 
 
