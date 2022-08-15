082ea135b6d653ca384a315c7d57e193cdb5d510
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-15 16:02:16 +0545

    Updated streamlit.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pandas import DataFrame
 st.title('Welcome to the Machine learning Model of Prosper Loan Data')
 st.subheader('Developed by Aashutosh Sinha')
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
+     if uploaded_file is not None:
      # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
      st.write(bytes_data)
@@ -22,7 +22,7 @@ if uploaded_file is not None:
      dataframe = pd.read_csv(uploaded_file)
      st.write(dataframe)
      uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
+     for uploaded_file in uploaded_files:
      bytes_data = uploaded_file.read()
      st.write("filename:", uploaded_file.name)
      st.write(bytes_data)
