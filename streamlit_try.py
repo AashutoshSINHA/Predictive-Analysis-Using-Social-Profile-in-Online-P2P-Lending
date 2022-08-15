c23c807d4a46a15fb48d6845e38381e200cd19ab
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-15 23:49:57 +0545

    Updated streamlit.py

--- streamlit_try.py
+++ streamlit_try.py
@@ -4,30 +4,7 @@ import numpy as np
 
 img_file_buffer = st.camera_input("Take a picture")
 
-if img_file_buffer is not Noimport streamlit as st
-with st.sidebar:
-    add_radio = st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-import streamlit as st
-from PIL import Image
-import numpy as np
-
-img_file_buffer = st.camera_input("Take a picture")
-
 if img_file_buffer is not None:
-    # To read image file buffer as a PIL Image:
-    img = Image.open(img_file_buffer)
-
-    # To convert PIL Image to numpy array:
-    img_array = np.array(img)
-
-    # Check the type of img_array:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(img_array))
-
-    # Check the shape of img_array:
-    # Should output shape: (height, width, channels)
-    st.write(img_array.shape)
-ne:
     # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
