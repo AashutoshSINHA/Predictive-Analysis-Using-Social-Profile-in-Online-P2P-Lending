22777949c558074c968ca32cf68874f2aaca4423
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-16 20:55:35 +0545

    Updated streamlit_try.py

--- streamlit_try.py
+++ streamlit_try.py
@@ -58,21 +58,3 @@ def main():
             
             
         st.write(f"The predicted LoanStatus is:  {LoanStatus}")
-
-
-if __name__ == '__main__':
-       main()    
-img_file_buffer = st.camera_input("Take a picture")
-
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-    # Check the type of cv2_img:
-    # Should output: <class 'numpy.ndarray'>
-    st.write(type(cv2_img))
-
-    # Check the shape of cv2_img:
-    # Should output shape: (height, width, channels)
-    st.write(cv2_img.shape)
