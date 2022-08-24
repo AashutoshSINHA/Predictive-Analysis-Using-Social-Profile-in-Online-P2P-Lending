8c51ae367bb55d691de819c8c4a3c7041fec944f
Āśutōṣa <www.aashutoshsinha888@gmail.com>
2022-08-24 14:22:40 +0545

    Update streamlit_try.py

--- streamlit_try.py
+++ streamlit_try.py
@@ -1,57 +1,24 @@
+# Smart dustbin
 import streamlit as st
-import pickle
-import pandas as pd
-import numpy as np
-from sklearn.naive_bayes import GaussianNB
-from prediction import get_prediction
+from streamlit_folium import st_folium
+import folium
 
+# center on Liberty Bell, add marker
+m = folium.Map(location=[27.66436936716696, 85.32659444366197], zoom_start=16)
+folium.Marker(
+    [27.66436936716696, 85.32659444366197], 
+    popup="Dustbin: RFID-8907-0980", 
+    tooltip="Dustbin: RFID-8907-0980"
+).add_to(m)
 
-st.set_page_config(page_title='Site Energy Intensity Prediction', page_icon="💸",
-                               layout="wide", initial_sidebar_state='expanded')
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width = 725)
 
-pickle_in = open('model.pkl', 'rb') 
-model = pickle.load(pickle_in)
-
-
-# creating option list for dropdown menu
-
-features = ['ListingNumber', 'ClosedDate', 'BorrowerAPR', 'BorrowerRate','DateCreditPulled', 'LoanMonthsSinceOrigination', 'LoanNumber',
-       'LoanOriginationQuarter', 'LP_CustomerPayments','LP_CustomerPrincipalPayments']
-
-st.markdown("<h1 style='text-align: center;'>Prosper LoanStatus Prediction App 💸 </h1>", unsafe_allow_html=True)
-
-
-def main():
-    with st.form('prediction_form'):
-
-        st.header("Predict the input for following features:") 
-                
-        
-        ListingNumber = st.selectbox( 'ListingNumber:', [1,4,6])
-        
-        ClosedDate = st.selectbox( 'ClosedDate:', [1,78,81,92,100,105])
-        BorrowerAPR = st.selectbox('BorrowerAPR:', [1,747,902,1260,1644,3672])
-        BorrowerRate = st.selectbox( 'BorrowerRate:', [1,1319,1508,1651,1905,3672])
-        DateCreditPulled = st.selectbox('DateCreditPulled:', [1,4,6])
-        LoanMonthsSinceOrigination = st.selectbox('LoanMonthsSinceOrigination:', [8,9,13,20,4336,4485,4899,5215,5865])
-        LoanNumber = st.selectbox('LoanNumber:', [1,4,6])
-        LoanOriginationQuarter = st.selectbox('LoanOriginationQuarter:', [13,585,1243,1270,1600,2403,3074,3913,4424,5061,14450])
-        LP_CustomerPayments = st.selectbox('LP_CustomerPayments:', [1,82,83,119,6208])
-        LP_CustomerPrincipalPayments = st.selectbox('LP_CustomerPrincipalPayments:', [1,1938,2042,2274,2595,6308])
-        
-        submit = st.form_submit_button("Predict")
-    
-    if submit:
-
-        data= np.array([ListingNumber, ClosedDate, BorrowerAPR, BorrowerRate, DateCreditPulled,LoanMonthsSinceOrigination,LoanNumber,
-                        LoanOriginationQuarter,LP_CustomerPayments,LP_CustomerPrincipalPayments]).reshape(1, -1)
-        
-        pred = get_prediction(data=data, model=model)
-
-        if pred == 0:
-            LoanStatus = 'Rejected'
-        elif pred == 1:
-            LoanStatus = 'Accepted'
-            
-            
-        st.write(f"The predicted LoanStatus is:  {LoanStatus}")
+    # Color negative values red; color significant p-value green and not significant red
+    table2 = ncol1.write(
+        metrics.style.format(
+            formatter={("p-value", "z-score"): "{:.3g}", ("uplift"): "{:.3g}%"}
+        )
+        .applymap(style_negative, props="color:red;")
+        .apply(style_p_value, props="color:red;", axis=1, subset=["p-value"])
+    )
